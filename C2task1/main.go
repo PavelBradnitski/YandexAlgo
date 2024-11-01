f1c36ab79b009898ad717a2cc5cd260dacbd400a
PavelBradnitski <pavel.brodnizkiy@mail.ru>
2024-11-01 15:25:54 +0300

    Fix Chapter2 Task1 to PrefixSum

--- C2task1/main.go
+++ C2task1/main.go
@@ -4,29 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
-	var len int
-	fmt.Fscanln(in, &len)
-	inputStr := make([]int, len)
-	for i := 0; i < len; i++ {
+	var length int
+	fmt.Fscanln(in, &length)
+	inputStr := make([]int, length)
+	for i := 0; i < length; i++ {
 		fmt.Fscan(in, &inputStr[i])
 	}
-	fmt.Fprint(out, strings.Join(PrefixSum(inputStr), " "))
+	numbers := PrefixSum(inputStr)
+	numbers = numbers[1:]
+	for i := 0; i < len(numbers); i++ {
+		fmt.Fprintf(out, "%d ", numbers[i])
+	}
 
 }
-func PrefixSum(nums []int) []string {
-	sum := 0
-	numsToStr := make([]string, len(nums))
+func PrefixSum(nums []int) []int {
+	numsToStr := make([]int, len(nums)+1)
 	for i, v := range nums {
-		sum += v
-		numsToStr[i] = strconv.Itoa(sum)
+		numsToStr[i+1] = numsToStr[i] + v
 	}
 	return numsToStr
 }
